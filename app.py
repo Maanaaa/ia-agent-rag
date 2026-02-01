d361f3af139f45bc23dc1429185e44c28ed0d18b
Mana <manya.th@icloud.com>
2026-02-01 12:14:37 +0100

    feat: transformation du script en un agent conversationnel interactif (cli)

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 import ollama
 import os # Permet de manipuler les chemins de fichiers
 
-def test_ollama():
-    print("Test de la connexion à GEMMA 3")
+def chatter():
+    # Identité de l'IA
+    instruction_agent = "Tu es un assistant expert en estimation de projets digitaux. Sois concis et pro."
+    print("Chatter avec l'IA")
+    print("(Tape quitter pour arrêter)")
 
-    prompt = "Réponds moi connexion réussie à GEMMA 3"
+    while True:
+        user_input = input("Vous : ")
 
-    try:
-        response = ollama.chat(model="gemma3:4b", messages=[{"role": "user", "content": prompt}])
-        print(f"Réponse : {response['message']['content']}")
+        if user_input.lower() == "quitter":
+            print("Stop")
+            break
+        
+        try:
+            # Envoyer la requête à l'IA
+            response = ollama.generate(
+                model="gemma3:4b",
+                system=instruction_agent,
+                prompt=user_input,
+            )
 
-    except Exception as e:
-        print(f"Erreur GEMMA 3 : {e}")
+            print(f"IA : {response['response']}\n")
+            # Séparation entre les messages
+            print("-"*30) 
+        except Exception as e:
+            print(f"Erreur : {e}")
+            break
 
 if __name__ == "__main__":
-    test_ollama()
+    chatter()
